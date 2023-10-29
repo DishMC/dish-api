badd5158f5d2c0bc684fba6ea53a097d3757835c
iris <60985521+KaiAF@users.noreply.github.com>
2023-10-29 11:39:28 -0500

    make PlayerChatEvent message a Component

--- src/main/java/net/ouja/api/event/player/PlayerChatEvent.java
+++ src/main/java/net/ouja/api/event/player/PlayerChatEvent.java
@@ -3,17 +3,18 @@ package net.ouja.api.event.player;
 import net.ouja.api.Player;
 import net.ouja.api.event.CancellableEvent;
 import net.ouja.api.event.EventListener;
+import net.ouja.api.network.chat.Component;
 
 public class PlayerChatEvent extends PlayerEvent implements EventListener, CancellableEvent {
     private boolean cancelled = false;
-    private String message;
+    private Component message;
 
-    public PlayerChatEvent(Player player, String message) {
+    public PlayerChatEvent(Player player, Component message) {
         super(player);
         this.message = message;
     }
 
-    public String getMessage() {
+    public Component getMessage() {
         return message;
     }
 
