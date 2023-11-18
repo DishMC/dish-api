a4dd6812394d205d4a3d07e57d09d5b92a1581d3
iris <60985521+KaiAF@users.noreply.github.com>
2023-11-17 22:28:42 -0600

    Add PlayerSleepEvent
    Added some missing Level wrappers: isDay, isNight, getGameTime, getDayTime, dimension. Need to add some more

---
+++ src/main/java/net/ouja/api/event/player/PlayerSleepEvent.java
@@ -0,0 +1,53 @@
+package net.ouja.api.event.player;
+
+import net.ouja.api.Player;
+import net.ouja.api.event.CancellableEvent;
+import net.ouja.api.event.EventListener;
+import net.ouja.api.world.level.block.BlockPos;
+
+public class PlayerSleepEvent extends PlayerEvent implements EventListener, CancellableEvent {
+    private boolean isCancelled = false;
+    private final BlockPos blockPos;
+    private boolean canSleepWhileMonstersAreNear = false;
+
+    public PlayerSleepEvent(Player player, BlockPos blockPos) {
+        super(player);
+        this.blockPos = blockPos;
+    }
+
+    public BlockPos getBlockPos() {
+        return blockPos;
+    }
+
+    /**
+     * Set if the player can sleep while there are monsters near
+     * @param b
+     */
+    public void setCanSleepWhileMonstersAreNear(boolean b) {
+        this.canSleepWhileMonstersAreNear = b;
+    }
+
+    /**
+     * If this is true, the player can sleep while there are monsters near
+     * @return if sleep is allowed near monsters
+     */
+    public boolean canSleepWhileMonstersAreNear() {
+        return this.canSleepWhileMonstersAreNear;
+    }
+
+    /**
+     * Controls whether the event will be cancelled or not
+     */
+    @Override
+    public void setCancel(boolean cancel) {
+        this.isCancelled = cancel;
+    }
+
+    /**
+     * @return the cancelled state
+     */
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+}
